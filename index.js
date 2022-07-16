48f42c485fba098853ab9b275929a0eaab685c09
paridaabinash <99640788+paridaabinash@users.noreply.github.com>
2022-07-16 16:53:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Que.1--Write one example explaining how you can write a callback function 
-//Que.5--Create examples to explain callback function.
 
 const sayHi = (nameOfFrirnd)=>{
     console.log("Hello my friend", nameOfFrirnd);
@@ -13,7 +12,6 @@ greeting("Abinash",sayHi);
 
 
 // //Que.2"Write callback function to print numbers from 1 to 7, in which 1 should be printed after 1 sec , 2 should be printed after 2 sec, 3 should be printed after 3 sec and so on. 
-//Que.6--Create examples to explain callback hell function.
 
 // // Explain callback hell.
 // // Numbers
@@ -101,8 +99,7 @@ greeting("Abinash",sayHi);
 // })
 
 //Que.4-- Create a promise function accepting a argument, if yes is passed to the function then it should go to resolved state and print Promise Resolved, and if nothing is passed then it should go to reject state and catch the error and print Promise Rejected 
-//Que.7--Create examples to explain promises function.
-
+  
 let promise1 = (arg) =>
 new Promise((resolve,reject)=>{
     
@@ -121,6 +118,60 @@ promise1("y").then(()=>{
     console.log("Promise is rejected")
 })
 
+//Que.5--Create examples to explain callback function.
+
+function coffee(){
+console.log("or chocolate coffee ");
+}
+function tea(callback1){
+    console.log("hot tea"); 
+callback1();
+}
+tea(coffee);
+
+//Que.6--Create examples to explain callback hell function.
+
+//  const callBackHell = () => {
+//      console.log("This is what callback hell looks like");
+//       setTimeout(() => {
+//           console.log("first callback");
+//           setTimeout(() => {
+//               console.log("second callback");
+//               setTimeout(() => {
+//                   console.log("third callback");
+//                   setTimeout(() => {                     
+//                      console.log("fourth callback");     
+//                      setTimeout(() => {
+//                          console.log("fifth callback");                 
+//                          setTimeout(() => {
+//                              console.log("sixth callback");
+//                              setTimeout(() => {
+//                                 console.log("seventh callback");
+//                              },1000);                           
+//                          },1000);
+//                      },1000);
+//                  },1000);
+//              },1000);
+//          },1000);        
+//      },1000);
+//  }
+//  callBackHell();
+
+//Que.7--Create examples to explain promises function.
+
+let myPromise= new Promise(function(Resolve,Reject){
+let x=1;
+
+if (x==1){
+    Resolve("OK")
+}else{
+    Reject("Error")
+}
+})
+myPromise.then(
+    function(value) {myDisplay(value);},
+)
+console.log(myPromise)
 
 //Que.8--Create examples to explain async await function
 
@@ -145,7 +196,28 @@ data.then((res)=>{
 console.log("End of code");
 
 
+//Que.9-- Create examples to explain promise.all function
 
+
+let pro1 = new Promise((resolve, reject) => {
+  resolve("first resolve");
+  reject("first error");
+});
+let pro2 = new Promise((resolve, reject) => {
+  resolve("second resolve");
+  reject("second error");
+});
+let pro3 = new Promise((resolve, reject) => {
+  resolve("third resolve");
+  reject("third error");
+});
+Promise.all([pro1, pro2, pro3])
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 
 
